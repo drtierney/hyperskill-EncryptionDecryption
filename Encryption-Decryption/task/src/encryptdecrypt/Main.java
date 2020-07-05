1a7cda05bfb76ed73c53baca49f405d06f51b5d7
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 11:13:01 +0100

    Stage #5 completed
    Stage #5 X-files
    Work with files; learn to store your data in a file.

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,58 +1,88 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        String mode = "enc";
-        int key = 0;
+        boolean encode = true;
+        String message = "";
         String data = "";
+        int shift = 0;
+        String inFile = "";
+        String inFileData = "";
+        String outFile = "";
+        String result;
 
-        for(int i = 0; i < args.length; i += 2){
-            switch (args[i]){
+
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
                 case "-mode":
-                    mode = args[i+1];
+                    encode = "enc".equals(args[i + 1]);
                     break;
                 case "-key":
-                    key = Integer.parseInt(args[i+1]);
+                    shift = Integer.parseInt(args[i + 1]);
                     break;
                 case "-data":
-                    data = args[i+1];
+                    data = args[i + 1];
+                    break;
+                case "-in":
+                    inFile = args[i + 1];
+                    break;
+                case "-out":
+                    outFile = args[i + 1];
                     break;
             }
+
+            // Check if inFile exists and parse input
+            if (!inFile.isBlank()) {
+                try (Scanner scanner = new Scanner(new File(inFile))) {
+                    inFileData = scanner.nextLine();
+                } catch (FileNotFoundException e) {
+                    System.out.println("File not found");
+                    break;
+                }
+            }
+
+            // if -data and -in both exist, prioritise -data
+            message = !data.equals("") ? data : inFileData;
+
+        }
+        if (encode) {
+            result = encode(message, shift);
+        } else {
+            result = decode(message, shift);
         }
 
-        String result = "";
-
-        switch (mode) {
-            case "enc":
-                result = encrypt(data, key);
-                break;
-            case "dec":
-                result = decrypt(data, key);
-                break;
-            default:
-                System.out.println("Unknown operation");
+        //Check if -out file has been included, if not go to std out
+        if (outFile.isBlank()) {
+            System.out.println(result);
+        } else {
+            try (FileWriter fileWriter = new FileWriter(new File(outFile))) {
+                fileWriter.write(result);
+                fileWriter.flush();
+            } catch (IOException e) {
+                System.out.println("Error writing result");
+            }
         }
-        System.out.println(result);
     }
 
-    public static String encrypt(String str, int shift) {
-        StringBuilder shiftedString = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char c = (char) (str.charAt(i) + shift);
-            shiftedString.append(c);
+    public static String encode(String str, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            result.append((char) (c + shift));
         }
-
-        return shiftedString.toString();
+        return result.toString();
     }
 
-    public static String decrypt(String str, int shift) {
-        StringBuilder shiftedString = new StringBuilder();
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = (char) (str.charAt(i) - shift);
-            shiftedString.append(c);
+    public static String decode(String str, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            result.append((char) (c - shift));
         }
-
-        return shiftedString.toString();
+        return result.toString();
     }
 }
